c49556011ce6b6ee6981d4fe8b8f2479e1ade090
Yukai Huang <yukaihuangtw@gmail.com>
2024-07-14 16:04:48 +0800

    fix: exercises block output

--- packages/liftledger-fmt/formatter.js
+++ packages/liftledger-fmt/formatter.js
@@ -33,29 +33,55 @@ function formatLiftLedger(input, indentSize = 2) {
   }
 
   function findMaxNameLength(node) {
-    switch (node.type) {
-      case 'source_file':
-      case 'template_block':
-      case 'log_entry':
-      case 'measurement_entry':
-      case 'pr_entry':
-      case 'exercises_block':
-        node.children.forEach(findMaxNameLength);
-        break;
-      case 'template_exercise':
-      case 'logged_exercise':
-      case 'measurement':
-      case 'pr_record':
-      case 'attribute':
-        maxNameLength = Math.max(maxNameLength, safeGetText(node, 0).length);
-        break;
+    if (node.type === 'attribute') {
+      const name = safeGetText(node, 0);
+      maxNameLength = Math.max(maxNameLength, name.length);
     }
+    node.children.forEach(findMaxNameLength);
+  }
+
+  function formatExercisesBlock(node) {
+    output += '@exercises\n';
+    indent();
+
+    for (let i = 1; i < node.children.length; i++) {
+      const child = node.children[i];
+      if (child.type === 'exercise') {
+        formatExercise(child);
+      }
+    }
+    dedent();
+    output += '@end-exercises\n\n';
+  }
+
+  function formatExercise(node) {
+    output += getIndent() + '[' + safeGetText(node, 1) + ']\n';
+    indent();
+    for (let i = 3; i < node.children.length; i++) {
+      const child = node.children[i];
+      if (child.type === 'attribute') {
+        formatAttribute(child);
+      }
+    }
+    dedent();
+    output += '\n';
+  }
+
+  function formatAttribute(node) {
+    const name = safeGetText(node, 0);
+    const value = safeGetText(node, 2);
+    const padding = ' '.repeat(Math.max(0, maxNameLength - name.length));
+    output += getIndent() + name + padding + ': ' + value + '\n';
   }
 
   function formatWithAlignment(node) {
     switch (node.type) {
       case 'source_file':
         node.children.forEach(formatWithAlignment);
+        break;
+
+      case 'exercises_block':
+        formatExercisesBlock(node);
         break;
 
       case 'template_block':
@@ -70,19 +96,12 @@ function formatLiftLedger(input, indentSize = 2) {
       case 'logged_exercise':
       case 'measurement':
       case 'pr_record':
-      case 'attribute':
         const name = safeGetText(node, 0);
-        const padding = ' '.repeat(maxNameLength - name.length);
+        const padding = ' '.repeat(Math.max(0, maxNameLength - name.length));
         output += getIndent() + name + padding + ': ';
-        if (node.type === 'template_exercise' || node.type === 'logged_exercise') {
-          output += safeGetText(node, 2);
-          if (node.children.length > 3) {
-            output += ' ' + safeGetText(node, 3);
-          }
-        } else if (node.type === 'pr_record') {
-          output += safeGetText(node, 2) + ' ' + safeGetText(node, 3);
-        } else {
-          output += safeGetText(node, 2);
+        output += safeGetText(node, 2);
+        if (node.children.length > 3) {
+          output += ' ' + safeGetText(node, 3);
         }
         output += '\n';
         break;
@@ -117,21 +136,6 @@ function formatLiftLedger(input, indentSize = 2) {
 
       case 'include':
         output += node.text + '\n\n';
-        break;
-
-      case 'exercises_block':
-        output += '@exercises\n';
-        indent();
-        node.children.slice(1, -1).forEach(formatWithAlignment);
-        dedent();
-        output += '@end-exercises\n\n';
-        break;
-
-      case 'exercise':
-        output += getIndent() + '[' + safeGetText(node, 0) + ']\n';
-        indent();
-        node.children.slice(2).forEach(formatWithAlignment);
-        dedent();
         break;
     }
   }
